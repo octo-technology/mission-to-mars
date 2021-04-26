70611c2f38fe633b514bc11bf080c7aa580d1573
EMEAAD\cbreheretgirardin <christophe.breheret-girardin@soprasteria.com>
2021-04-26 19:35:11 +0200

    Retour à la version non SOLID

--- mission-to-mars-solid/src/main/java/mars/Helicoptere.java
+++ mission-to-mars-solid/src/main/java/mars/Helicoptere.java
@@ -1,7 +1,5 @@
 package mars;
 
-import java.util.stream.IntStream;
-
 public class Helicoptere extends Rover implements ModuleMartien3D {
 
 	public Helicoptere(Direction direction, Position position) {
@@ -18,14 +16,4 @@ public class Helicoptere extends Rover implements ModuleMartien3D {
 		position = new Position(position.getX(), position.getX(), position.getZ() - 1);
 	}
 
-	@Override
-	public void activerRecuperation() {
-		atterir();
-		super.activerRecuperation();
-	}
-	
-	private void atterir() {
-		IntStream.range(0, getPosition().getZ())
-			.forEach((i -> descendre()));
-	}
 }
