f8f54ee2b3a2ba9b46f723103b4d948cc484b2eb
Fabien Mercier <fabien.mercier@octo.com>
2023-11-27 09:43:43 +0100

    Renommer curiosity par perseverance

--- java/mission-to-mars-solid/src/test/java/mars/SondeTest.java
+++ java/mission-to-mars-solid/src/test/java/mars/SondeTest.java
@@ -11,14 +11,14 @@ class SondeTest {
 	void roverDoitEtreRecupere() {
 		// Arrange
 		Sonde sut = new Sonde();
-		ModuleMartien2D curiosity = new Rover(Direction.NORD, new Position(4, 4, 0));
+		ModuleMartien2D perseverance = new Rover(Direction.NORD, new Position(4, 4, 0));
 
 		// Act
-		sut.preparerRecuperation(curiosity);
+		sut.preparerRecuperation(perseverance);
 
 		// Assert
-		assertTrue(curiosity.isPretPourRecuperation());
-		assertEquals(new Position(4, 4, 0), curiosity.getPosition());
+		assertTrue(perseverance.isPretPourRecuperation());
+		assertEquals(new Position(4, 4, 0), perseverance.getPosition());
 	}
 
 	@Test
